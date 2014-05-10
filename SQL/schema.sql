44eadccdad75730ad1a2ad04f311c94e50ca4199
Hack Reactor Students <keiwi.g@gmail.com>
2014-05-09 17:28:24 -0700

    database schema defined. client and server files available

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -4,7 +4,33 @@ USE chat;
 
 CREATE TABLE messages (
  /* Describe your table here.*/
+ id int(10),
+ date datetime,
+ text blob,
+ user int(10),
+ primary key (id)
 );
+
+CREATE TABLE users (
+ /* Describe your table here.*/
+ id int(10),
+ name varchar(100),
+ messages blob,
+ primary key (id)
+);
+
+CREATE TABLE rooms (
+ /* Describe your table here.*/
+ id int(10),
+ messages blob,
+ name varchar(100),
+ primary key (id)
+);
+
+ALTER TABLE users MODIFY id int(10) auto_increment;
+ALTER TABLE rooms MODIFY id int(10) auto_increment;
+ALTER TABLE messages MODIFY id int(10) auto_increment;
+
 
 /* You can also create more tables, if you need them... */
 
